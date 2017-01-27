1ab81bf93123fe7afb8fbd7f82058b84c98f0656
Ravi Soji <raviisojitraa@gmail.com>
2017-01-27 17:09:17 -0500

    decode_mpeg works now works for stride > batch_size

--- image-analysis/decode/video_decoder.py
+++ image-analysis/decode/video_decoder.py
@@ -1,14 +1,34 @@
-# REQUIRED PACKAGES
-# sci-kit-image
-# ffmpeg
-# skvideo
-
 import os
 import skvideo.io
 import numpy as np
 from skimage.io import *
 from skimage.color import rgb2gray
 
+def pad_batch(batch, batch_size, frame):
+    """
+    INPUTS
+    frame:      a numpy array extracted from an MPEG (length x width x channels)
+    batch:      list of frames
+    batch_size: number of frames per batch (integer >= 1)
+
+    OUTPUT
+    returns padded batch: list of batch_size frames, each an ndarray:(L x W x C)
+
+    DESCRIPTION: takes in a batch, pads it with 0s if necessary, and returns 
+                    appended batch_list
+    """
+    if len(batch) > batch_size:
+        raise ValueError("len(batch) should be <= batch_size!")
+    elif batch_size < 1:
+        raise ValueError("batch_size must be >= 1")
+    elif len(batch) == batch_size:
+        return batch
+    elif len(batch) < batch_size:
+        pad_list = [np.zeros(frame.shape)] * (batch_size - len(batch))
+        batch += pad_list
+        return batch
+    else:
+        raise ValueError("Something is wrong with the pad_batch function")
 
 def decode_mpeg(v_path,*, batch_size=1, stride=1, start_idx=0, end_idx=-1,
         out_frame_ext=".jpg", out_frame_dir=""):
@@ -42,50 +62,39 @@ def decode_mpeg(v_path,*, batch_size=1, stride=1, start_idx=0, end_idx=-1,
     batch_list = []
     count = 0
     temp = start_idx
-    
-#    initialize batch
-#    if stride >= batch_size:
-#        batch = []
-#    elif batch_size > stride:
-#        for frame in skvideo.io.reader(v_path):
-#            if count - start_idx  == batch_size:
-#                break
-#            elif count >= start_idx:
-#                batch.append(frame)
-#            count += 1
-
-# build batch_list
+    batch = []
+#   build batch_list
     count = 0
     for frame in skvideo.io.vreader(v_path):
         if count >= start_idx:
             if stride > batch_size:
-                if count % stride  == 0:
+                if count == end_idx:
+                    if len(batch) == batch_size:
+                        batch_list.append(np.array(batch))
+                        batch = []
+                    batch.append(frame)
+                    batch_list.append(np.array(pad_batch(batch, batch_size, frame)))
+                    return batch_list[1:]
+                elif count % stride  == 0:
                     temp = count
                     batch_list.append(np.array(batch))
                     batch = []
                     batch.append(frame)
-                elif (temp < count and count < (temp + batch_size)):
+                elif temp < count and count < (temp + batch_size):
                     batch.append(frame)
-                else print("problem")
-                if count == end_idx:
-                    if len(batch) == batch_size:
-                        batch_list.append(np.array(batch))
-                    elif len(batch) < batch_size:
-                        pad_list = [[np.zeros(frame.shape)]] * \
-                                (batch_size - len(batch))
-                        batch += pad_list
-                        batch_list.append(np.array(batch))
-                    else print("problem!!")
             elif batch_size >= stride:
+                print("batch_size >= stride")
                 if len(batch) < batch_size - stride:
                     batch.append(frame)
                 elif len(batch) % batch_size == 0:
+                    print("len(batch) % batch_size == 0")
                     batch_list.append(np.array(batch))
                     batch = []
                     for x in range(stride - batch_size, 0):
                         batch.append(batch_list[-1][x])
                 elif len(batch) < batch_size:
                     batch.append(frame)
+                else: print("problem")
                 if count == end_idx:
                     if len(batch) == batch_size:
                         batch_list.append(np.array(batch))
@@ -94,6 +103,15 @@ def decode_mpeg(v_path,*, batch_size=1, stride=1, start_idx=0, end_idx=-1,
                                 (batch_size - len(batch))
                         batch += pad_list
                         batch_list.append(np.array(batch))
-                    else print("problem!!")
+                    else: print("problem!!")
         count += 1
     return batch_list # list of numpy arrays
+
+
+curr_dir = os.getcwd()
+v_path = curr_dir + "/1.E.E.1.mp4"
+a = decode_mpeg(v_path, batch_size=2, stride=4, start_idx = 0, end_idx = 20)
+
+print(len(a))
+print(a[-1].shape)
+print(a[-1][-1,-1,-1])
