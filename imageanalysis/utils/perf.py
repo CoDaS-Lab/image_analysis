7c0431a3141f53bab9bbc689544cca1bfef1f727
Anderson Reyes <reyesanderson428@gmail.com>
2017-03-09 13:46:14 -0500

    fixes #4
    To use it just import the timeit() in perf.py and use it as a decorator
    ex. 
    @timeit(thresh)
    def func
    
    it prints out the timing of the function and if thresh is defined then it will print a statement if the function is slower than thresh

---
+++ imageanalysis/utils/perf.py
@@ -0,0 +1,23 @@
+import time
+from functools import wraps
+
+
+def timeit(thresh=None):
+    def decorator(method):
+        @wraps(method)
+        def timed(*args, **kw):
+            start = time.time()
+            result = method(*args, **kw)
+            elapsed = time.time() - start
+
+            print('\n{0}() timing: {1:.5f} sec'
+                  .format(method.__name__, elapsed))
+
+            if thresh is not None and elapsed > thresh:
+                print('{0}() ran slower than thresh={1}'
+                      .format(method.__name__, thresh))
+            return result
+        return timed
+    return decorator
+
+    return timed
