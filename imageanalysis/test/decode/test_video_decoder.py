4765fa1f1dcdd210e8653dc6031167bf6c82689e
Anderson Reyes <reyesanderson428@gmail.com>
2017-03-14 16:35:02 -0400

    pep8 formatting

--- imageanalysis/test/decode/test_video_decoder.py
+++ imageanalysis/test/decode/test_video_decoder.py
@@ -57,7 +57,7 @@ class TestVideoDecoder(unittest.TestCase):
 
             self.assertEqual(nframes, vd_nframes, message + '\nnframes ' +
                              '= {0}, vd_nframes = {1}'.format(
-                              self.nframes, vd_nframes))
+                                 self.nframes, vd_nframes))
             self.assertEqual(self.height, vd_height, message +
                              '\nself.height = {0}, vd_height = {1}'.format(
                                  self.height, vd_height))
@@ -243,15 +243,16 @@ class TestVideoDecoder(unittest.TestCase):
         prompt = 'batch_size < stride test: check last batch, first frame'
         test_mpeg_dimensions(prompt, nframes=numframes, vd_batch=batch)
         test_mpeg_frame(prompt, vd_frame=batch[0],
-                        frame=self.correct_data[start +
-                         b_stride * ((end - start + 1) // b_stride)])
+                        frame=self.correct_data[
+                            start + b_stride * ((end - start + 1) // b_stride)])
         prompt = 'batch_size < stride test: check last batch, ' + \
                  'last \"real frame\"'
         test_mpeg_frame(prompt, vd_frame=batch[-(((end - start + 1) % b_stride)\
                         > 0) * (((end - start + 1) % 15) - numframes)],
-                        frame=self.correct_data[-1 - (((end-start+1) % b_stride\
-                        - numframes) > 0) * ((end - start + 1) % b_stride\
-                        - numframes)])
+                        frame=self.correct_data[-1 - (((end - start + 1) %
+                                                b_stride - numframes) > 0) *
+                                                ((end - start + 1) % b_stride -
+                                                numframes)])
 
         # TODO: Test batch_size < stride, with padding off.
 
