820b0907f4aacf48ce721c2832c08dff4cd005a3
Ravi Soji <raviisojitraa@gmail.com>
2017-03-20 16:16:55 -0400

    Fixed test data link.

--- imageanalysis/test/extract/test_feature_extractor.py
+++ imageanalysis/test/extract/test_feature_extractor.py
@@ -9,7 +9,7 @@ from test.extract import test_features as features
 class TestFeatureExtractor(unittest.TestCase):
     def setUp(self):
         data_dir = 'test/test_data/'
-        vid_link = 'https://s3.amazonaws.com/testcodas/test_video.mp4'
+        vid_link = 'https://s3.amazonaws.com/codasimageanalysis/test_video.mp4'
         if not os.path.exists(data_dir + 'test_video.mp4'):
             wget.download(vid_link, data_dir)
 
