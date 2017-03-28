eaee853a6174488946e7d4686500ad5698309e2c
Anderson Reyes <reyesanderson428@gmail.com>
2017-03-28 18:39:52 -0400

    fixes #8

---
+++ decode/utils.py
@@ -0,0 +1,95 @@
+import time
+import os
+import numpy as np
+import skimage.io
+from functools import wraps
+
+_default_ext = ('.jpg', '.png')
+
+
+def timeit(thresh=None, classname=''):
+    """
+    DESCRIPTION:
+        python decorator to time functinos
+
+    PARAMS:
+        thresh: minimum speed allowed. Throws error if function is slower
+                than thresh
+        classname: class where the function you timing is located
+
+    RETURN:
+        skiamge.io.ImageCollection object that can be indexed as a list.
+        Elements in this 'list' are numpy arrays of image values
+
+    """
+    def decorator(method):
+        @wraps(method)
+        def timed(*args, **kw):
+            start = time.time()
+            result = method(*args, **kw)
+            elapsed = time.time() - start
+
+            print('\n {0}: {1}() timing: {2:.5f} sec'
+                  .format(classname, method.__name__, elapsed))
+
+            if thresh is not None and elapsed > thresh:
+                print('{0}() ran slower than thresh={1}'
+                      .format(method.__name__, thresh))
+            return result
+        return timed
+    return decorator
+
+
+def load_mult_images(dirname, exts=None, batchsize=1):
+    """
+    DESCRIPTION:
+        Load multiple images at once into batches
+
+    PARAMS:
+        dirname: directory of the images
+        exts: acceptable file extensions (must be a tuble)
+        batchsize: size of the batches
+
+    RETURN:
+        skiamge.io.ImageCollection object that can be indexed as a list.
+        Elements in this 'list' are numpy arrays of image values
+
+    """
+
+    if dirname is None or os.path.exists(dirname) is False:
+        raise ValueError('dirname: {0} is invalid'.format(dirname))
+
+    if batchsize <= 0:
+        raise ValueError('batchsize: {0} is invalid'.format(batchsize))
+
+    if exts is None:
+        exts = _default_ext
+    elif not isinstance(exts, tuple):
+        raise ValueError('exts: {0} is invalid'.format(exts.__class__))
+
+    batch_list = []
+    batch = []
+    for fname in sorted(os.listdir(dirname)):
+        if fname.endswith(exts):
+            absolute_fname = dirname + fname
+            # load frame add it to batch
+            batch.append(skimage.io.imread(absolute_fname))
+
+        if len(batch) == batchsize:
+            batch_list.append(batch)
+            batch = []
+
+    # padd last batch (if needed) to keep all batches the same size
+    if len(batch) < batchsize:
+        padsize = batchsize - len(batch)
+        padlist = [np.zeros(shape=batch_list[0][0].shape)] * padsize
+        batch += padlist
+        batch_list.append(batch)
+
+    return batch_list
+
+
+if __name__ == '__main__':
+    dirname = os.getcwd() + '/test/test_data/testimgs/'
+    imgs = load_mult_images(dirname, batchsize=2)
+    print(len(imgs))
---
+++ test/decode/test_utils.py
@@ -0,0 +1,36 @@
+import unittest
+import os
+import numpy as np
+import skimage.io
+from decode import utils
+
+
+class TestUtils(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_load_mult_imgs(self):
+        img_dir = os.getcwd() + '/test/test_data/testimgs/'
+        testimg1 = skimage.io.imread(img_dir + 'testimg1.jpg')
+        testimg2 = skimage.io.imread(img_dir + 'testimg2.jpg')
+        imgs = utils.load_mult_images(dirname=img_dir)
+
+        self.assertTrue(np.array_equal(testimg1, imgs[0][0]))
+        self.assertTrue(np.array_equal(testimg2, imgs[1][0]))
+
+    def test_load_mult_imgs_batches(self):
+        batchsize = 2
+        img_dir = os.getcwd() + '/test/test_data/testimgs/'
+        imgs = utils.load_mult_images(dirname=img_dir, batchsize=batchsize)
+
+        # 2 batches each with two images inside. Last batch should be padded
+        self.assertEqual(len(imgs), 2)
+        self.assertEqual(len(imgs[0]), batchsize)
+        self.assertEqual(len(imgs[1]), batchsize)
+
+if __name__ == '__name__':
+    unittest.main()
