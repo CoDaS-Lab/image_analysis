025402b36a160e2b023b41e37f6b8387cee71092
Anderson <reyesanderson428@gmail.com>
2017-04-18 18:14:29 -0400

    updated comments on how pipeline should work

--- demo/pipeline_demo.py
+++ demo/pipeline_demo.py
@@ -1,43 +1,18 @@
-import os
-import sys
-sys.path.append(os.getcwd() + "/../")
-import skimage.io
-import wget
-from matplotlib import pyplot as plt
-from decode import video_decoder as vd
-from extract import feature_extractor as fe
-from demo_features import RGBToGray
-from extract.orientation_filter import OrientationFilter
-from demo_features import BatchOP
-from extract.pipeline import Pipeline
+# NOTE: How pipeline should work
 
+# TODO make pipeline like this
+# pipeline = Pipeline([bowtie,rgb2gray, feat3], save=True)
+# pipeline = Pipeline([
+#                     feature4(
+#                     feature3(
+#                     feature2(
+#                     pipeline)))
+#                     ])
+# NOTE: extract_features(*) in feature_extractor.py  needs to account for params: 
+# oplist, batch_list,  dictionary d.s.
 
-vid_path = os.getcwd() + '/../test/test_data/'
-data = vd.decode_mpeg(vid_path + 'test_video.mp4',
-                      batch_size=2, end_idx=9, stride=2)
+# NOTE: in Feature class extract method should be implemented checking for save
+# variable and checking data type of the input, the input can be either a feature
+# or a pipeline data dictionary
 
-bowtie = OrientationFilter(mask='bowtie')
-
-bpipe = Pipeline(data=data,
-                 save=False,
-                 parallel=False,
-                 operations=[bowtie],
-                 models=None)
-# extract information or transform data by calling:
-pipeline_ouput = bpipe.transform()
-# the pipeline also saves the data internally for later access
-# you can also access the data as numpy array by:
-# bpipe.data_as_nparray()
-
-
-# shape (batches, number of frames in batches, number of features per frame)
-# grab first frame in first batch
-frame = pipeline_ouput[0][0]
-
-print(frame['input'].keys())
-print(frame['metadata'].keys())
-
-
-img = frame['input'][bowtie.key_name]
-skimage.io.imshow(img)
-plt.show()
+# NOTE: extract method has to call model method of input feature object
