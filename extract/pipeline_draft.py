6571990853399ee0a487aa50828344b685336752
Ravi Soji <raviisojitraa@gmail.com>
2017-04-26 22:41:04 -0400

    worked on as_ndarry method.

--- extract/pipeline_draft.py
+++ extract/pipeline_draft.py
@@ -17,6 +17,7 @@ class Pipeline:
         self.empty_frame = {}           # Defined by set method below.
         self.output = []          # Output data structure of the pipeline.
 
+
     def set_ops(self, ops, seq):
         batch_ops = []
         frame_ops = []
@@ -143,10 +144,24 @@ class Pipeline:
             # Still need to implement something that accoutns for save vars.
 
 
-    def as_ndarray(self, key='output'):
+    def as_ndarray(self, frame_key=None, batch_key=None, seq_key=None):
+        keys = [frame_key] + [batch_key] + [seq_key]
+        
+        key_count = 0
+        for x in keys:
+            if x is not None:
+                key_count += 1
+        if key_count != 1:
+            raise ValueError('One and only one of the three keys may be set.')
+        
+        elif keys[0] is not None:
+            asdf
+        elif keys[1] is not None:
+
+        
         data = []
         for frame_dict in self.output:
-            data.append(self.output[key])
+            data.append(self.frame_dict[key])
         
         return np.array(data)
 
@@ -156,130 +171,3 @@ class Pipeline:
 
     def train(self, model=''):
         pass
-
-
-    def gen_batch_features(batch_list, batch_op_list):
-        """
-        DESCRIPTION:
-            return list of dictionaries, with each dictionary
-            representing a batch and containing features
-            extracted according to batch_op_list
-    
-        PARAMS:
-            batch_list: list of batches (ndarrays)
-            batch_op_list: list of batch_op objects that extract features
-                        from batches
-    
-        RETURN:
-            batch_dictionaries: list of dictionaries, each representing a batch
-        """
-        batch_dictionaries = []
-        for batch in batch_list:
-            batch_ds = {
-                'input': {
-                    'batch': batch
-                },
-                'metadata': {
-    
-                }
-            }
-            batch_dictionaries.append(batch_ds)
-            for op in batch_op_list:
-                batch_ds['input'].update({op.key_name: op.extract(batch)})
-        return batch_dictionaries
-    
-    
-    def gen_frame_features(batch_list, frame_op_list):
-        """
-        DESCRIPTION:
-            return list of dictionaries, with each dictionary
-            representing a frame and containing features extracted
-            according to frame_op_list
-    
-        PARAMS:
-            batch_list: list of batches (numpy arrays)
-            batch_op_list: list of batch_op objects that extract features from
-                        batches
-    
-        RETURN:
-            frame_dictionaries: list of dictionaries, each representing a frame
-        """
-        frame_dictionaries = []
-        for batch in batch_list:
-            for frame in batch:
-                frame_ds = {
-                    'input': {
-                        'frame': frame
-    
-                    },
-                    'metadata': {
-                    }
-                }
-    
-                frame_dictionaries.append(frame_ds)
-                for op in frame_op_list:
-                    frame_ds['input'].update({op.key_name: op.extract(frame)})
-        return frame_dictionaries
-    
-    
-    def batch_to_frame_dictionaries(batch_dictionaries):
-        """
-        DESCRIPTION:
-            take in list of batch dictionaries and output list of
-            frame dictionaries
-    
-        PARAMS:
-            batch_dictionary: list of dictionaries representing batches (numpy
-                            arrays) of frames; each dictionary should have same
-                            exact keys, and each batch should be an ndarry
-    
-        RETURN:
-            frame_dictionary: list of dictionaries representing frames
-        """
-        frame_dictionaries = []
-        batch_index = 0
-        for batch_ds in batch_dictionaries:
-            for frame in batch_ds['input']['batch']:
-                frame_ds = {
-                    'input': {
-                        'frame': frame
-    
-                    },
-                    'metadata': {
-                        'batch_index': batch_index
-                    }
-                }
-    
-                frame_dictionaries.append(frame_ds)
-                for batch_op in batch_ds['input']:
-                    if batch_op != 'batch':
-                        frame_ds['input'][batch_op] = batch_ds['input'][batch_op]
-            batch_index += 1
-    
-        return frame_dictionaries
-    
-    
-    def extract_features(batch_ops, frame_ops):
-        """
-        DESCRIPTION:
-    
-        PARAMS:
-    
-        RETURN:
-            frame_dictioanries: list of dictionaries, each representing a frame
-        """
-        self.output = []
-        count = 0
-        for batch in batch_list:
-            batch_dictionary = gen_batch_features([batch], batch_ops)
-            frame_dictionaries += (batch_to_frame_dictionaries(batch_dictionary))
-    
-            if len(frame_ops) != 0:
-                for frame in batch:
-                    frame_dictionary = gen_frame_features([[frame]], frame_ops)
-    
-                    # add frame features
-                    for feat, val in frame_dictionary[0]['input'].items():
-                        frame_dictionaries[count]['input'][feat] = val
-                    count += 1
-        return frame_dictionaries
