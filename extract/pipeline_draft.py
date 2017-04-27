fe9777f837bf49cc09c8c188e71311f0fda32410
Ravi Soji <raviisojitraa@gmail.com>
2017-04-27 08:07:10 -0400

    Made Pipeline class sufficient and runnable.

--- extract/pipeline_draft.py
+++ extract/pipeline_draft.py
@@ -1,9 +1,10 @@
 import numpy as np
-from feature import Feature
+from extract.feature import Feature
+import copy
 
 
 class Pipeline:
-    def __init__(self, ops=None, data=None, seq=None, save_all=None,
+    def __init__(self, data=None, ops=None, seq=None, save_all=None,
                  models=None):
         self.data = data
         self.models = models      # Should be a list of models.
@@ -14,12 +15,12 @@ class Pipeline:
         self.batch_ops = None
         self.frame_ops = None
         self.seq_ops = None
-        self.set_op_lists(ops, seq)
+        self.set_ops(ops, seq)
 
         self.empty_frame = {}           # Defined by set method below.
         self.output = []          # Output data structure of the pipeline.
 
-    def set_ops(self, ops, seq):
+    def set_ops(self, ops=None, seq=None):
         batch_ops = []
         frame_ops = []
         if ops is not None:
@@ -46,9 +47,6 @@ class Pipeline:
         self.frame_ops = frame_ops
         self.seq_ops = seq
 
-    def reset_ops(self, ops=None, seq=None):
-        self.set_ops(ops, seq)
-
     def set_batch_ops(self, batch_ops=None):
         if batch_ops is None:
             batch_ops = []
@@ -72,7 +70,7 @@ class Pipeline:
     def set_seq(self, seq=None):
         for op in seq_ops:
             assert isinstance(Feature)
-        # Need to handle case of someone sending a batch into frame op 
+        # Need to handle case of someone sending a batch into frame op
         # & vise versa
         self.seq_ops = seq_ops
 
@@ -82,63 +80,64 @@ class Pipeline:
         assert isinstance(seq_ops, list)
 
         frame = {
-            'input': None,
-            'meta_data': None,
+            'input': {},
+            'meta_data': {},
             'batch_features': {},
             'frame_features': {},
-            'seq': None,
+            'seq': {},
             'seq_features': {},
-            'seq_output': None}
+            'seq_output': {}}
 
-        for op in batch_features:
-            frame['batch_features'].update(op.key: None)
+        for op in batch_ops:
+            frame['batch_features'].update({op.key_name: None})
         for op in frame_ops:
-            frame['frame_features'].update(op.key: None)
+            frame['frame_features'].update({op.key_name: None})
         for op in seq_ops:
-            frame['seq_features'].update(op.key: None)
+            frame['seq_features'].update({op.key_name: None})
 
         self.empty_frame = frame
 
     def extract(self, keep_input_data=True):
         if self.batch_ops == self.frame_ops == self.seq_ops == []:
             raise ValueError('No features were specified for extraction.')
-        elif self.seq_ops == []:
             self.extract_nonseq(self.data, self.batch_ops, self.frame_ops)
-        elif self.seq_ops != []:
-            self.extract_seq(self.data, self.seq_ops)
-        else:
-            raise ValueError('Someone broke the Pipeline class code.')
 
-        if keep_input_data is False:
-            self.data = []
-
-    def extract_nonseq(self, data, batch_ops, frame_ops, seq_ops):
-        self.set_frame(batch_ops, frame_ops, seq_ops)
+        self.set_empty_frame(self.batch_ops, self.frame_ops, self.seq_ops)
+        self.output = []
         n_frame = 0
         n_batch = 0
 
         for batch in self.data:
             batch_dict = {}
-            for op in batch_ops:
+            for op in self.batch_ops:
                 batch_dict.update({op.key_name: op.extract(batch)})
 
             for frame in batch:
-                frame_dict = self.empty_frame
-                for op in frame_ops:
+                frame_dict = copy.deepcopy(self.empty_frame)
+                frame_dict['input'] = frame
+                for op in self.frame_ops:
                     frame_dict['frame_features'][op.key_name] = op.extract(
                         frame)
-                    frame_dict['batch_features'].update(batch_dict)
+                frame_dict['batch_features'].update(batch_dict)
                 frame_dict['meta_data'].update({'frame_number': n_frame,
                                                'batch_number': n_batch})
                 self.output.append(frame_dict)
                 n_frame += 1
             n_batch += 1
 
-    def extract_sequence(self, seq_ops):
-        for op in seq_ops:
-            if op.save:
-                # Still need to implement something that accoutns for save vars
-                pass
+        if self.seq_ops != [] and self.seq_ops is not None:
+            for frame in self.output:
+                temp = frame['input']
+                for op in self.seq_ops:
+                    temp = op.extract(temp)
+                    if op.save is True or self.save_all is True:
+                        frame['seq_features'].update({op.key_name: temp})
+            frame['seq_output'] = temp
+
+        if keep_input_data is False:
+            self.data = []
+
+        return self.output
 
     def as_ndarray(self, frame_key=None, batch_key=None, seq_key=None):
         keys = [frame_key] + [batch_key] + [seq_key]
@@ -150,13 +149,18 @@ class Pipeline:
         if key_count != 1:
             raise ValueError('One and only one of the three keys may be set.')
 
-        # elif keys[0] is not None:
-        #     asdf
-        # elif keys[1] is not None:
-
         data = []
-        for frame_dict in self.output:
-            data.append(self.frame_dict[key])
+        if frame_key is not None:
+            for frame_dict in self.output:
+                data.append(frame_dict['frame_features'][frame_key])
+        elif batch_key is not None:
+            for frame_dict in self.output:
+                data.append(frame_dict['batch_features'][batch_key])
+        elif seq_key is not None:
+            for frame_dict in self.output:
+                data.append(frame_dict['seq_features'][seq_key])
+        else:
+            raise ValueError('Someone broke the as_ndarray method!')
 
         return np.array(data)
 
@@ -165,3 +169,13 @@ class Pipeline:
 
     def train(self, model=''):
         pass
+
+    def display(self):
+        print('This method will not print input data.')
+        print('models: {}'.format(self.models))
+        print('save_all: {}'.format(self.save_all))
+        print('batch_ops: {}'.format(self.batch_ops))
+        print('frame_ops: {}'.format(self.frame_ops))
+        print('seq_ops: {}'.format(self.seq_ops))
+        print('empty frame: {}'.format(self.empty_frame))
+        print('This method will not print output data.')
