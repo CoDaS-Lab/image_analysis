72d95fd7ee18d1691055c739daf49823805f728f
Anderson Reyes <reyesanderson428@gmail.com>
2017-04-27 13:13:10 -0400

    bowtie test with sinewave

--- test/extract/test_orientation_filter.py
+++ test/extract/test_orientation_filter.py
@@ -4,7 +4,12 @@ import wget
 import time
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
+import skimage.io
 from decode import video_decoder as vd
+from pyfftw.interfaces.numpy_fft import fftshift
+from pyfftw.interfaces.numpy_fft import fft2
+from pyfftw.interfaces.numpy_fft import ifft2
 from extract.orientation_filter import OrientationFilter
 
 
@@ -32,18 +37,22 @@ class TestFeatureExtractor(unittest.TestCase):
 
         x = y = np.linspace(-wDeg // 2, wDeg // 2, nPix + 1)
         u, v = np.meshgrid(x, y)
-        # u = u[:-2, :-2]
-        # v = v[:-2, :-2]
 
         ramp = np.sin(orientation * np.pi / 180) * u
         ramp -= np.cos(orientation * np.pi / 180) * v
         sinwaves_grating = np.sin(2 * np.pi * sf * ramp)
 
         mask_filter = OrientationFilter(mask='bowtie', falloff='triangle')
-        filt = mask_filter.bowtie(90, 20, nPix, .1, nPix + 1, 'triangle')
+        filt = mask_filter.bowtie(90, 42, nPix, .1, nPix + 1, 'triangle')
 
-        # TODO finish this multiply filt by the sinwaves_grating and it should
-        # make the sine wave disappear
+        fft_grating = fft2(sinwaves_grating)
+        filt_shift = fftshift(filt)
+
+        out = fft_grating * filt_shift
+        altered = ifft2(out).real
+        skimage.io.imshow_collection([sinwaves_grating, altered])
+        # plt.imshow(altered, cmap='gray')
+        plt.show()
 
 
 if __name__ == '__name__':
