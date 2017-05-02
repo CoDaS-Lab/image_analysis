12ca284a3e4f36f738a2a74ef9c54c54095ee263
Anderson Reyes <reyesanderson428@gmail.com>
2017-05-01 22:21:06 -0400

    finished test_as_ndarray()

--- test/extract/test_pipeline.py
+++ test/extract/test_pipeline.py
@@ -91,7 +91,6 @@ class TestPipeline(unittest.TestCase):
         for frame in pipeline_ouput:
             op_keys = list(frame['frame_features'].keys())
             self.assertEqual(len(op_keys), 0)
-            print(frame)
             self.assertIsNotNone(frame['seq_features'][maxPixel.key_name])
             self.assertIsNotNone(frame['input'])
 
@@ -100,39 +99,26 @@ class TestPipeline(unittest.TestCase):
             self.assertIsNotNone(frame['meta_data']['frame_number'])
             self.assertIsNotNone(frame['meta_data']['batch_number'])
 
-    def test_create_dict(self):
-        fake_transformations = {'test1': 123456}
-        fake_metadata = {'fake_metadata': 7890}
-        testpipe = Pipeline()
-        fake_dict = testpipe.create_dict(transforms=fake_transformations,
-                                         metadata=fake_metadata)
-
-        keys = list(fake_dict.keys())
-        # only input and metadata in dict
-        self.assertEqual(len(keys), 2)
-        self.assertIsNotNone(fake_dict['input']['test1'])
-        self.assertIsNotNone(fake_dict['metadata']['fake_metadata'])
-
-    def test_data_as_nparray(self):
+    def test_as_ndarray(self):
+        # TODO implement this function
         data = vd.decode_mpeg(self.vid_path, batch_size=2, end_idx=9,
                               stride=2)
 
+        frame_width = data[0][0].shape[0]
+        frame_height = data[0][0].shape[1]
+        n = len(data) * len(data[0])  # n = numbatches * framesperbatch
+
         rgb2gray = features.RGBToGray()
         maxPixel = features.ArgMaxPixel()
-        batchNum = features.BatchOP()
 
         testpipe = Pipeline(data=data,
-                            save=True,
-                            parallel=True,
-                            operations=[rgb2gray, maxPixel, batchNum],
-                            models=None)
-        # extract information or transform data by calling:
-        testpipe.transform()
-        # get data as np arrays
-        pipeline_ouput = testpipe.data_as_nparray()
-        # shape should be (5, 2, 4)-> 5 batches with 2 frames each and 4
-        # feature maps per frame (3 extracted features above + original frame)
-        self.assertTupleEqual(pipeline_ouput.shape, (5, 2, 4))
+                            ops=[rgb2gray, maxPixel],
+                            save_all=True)
+
+        pipeline_ouput = testpipe.extract()
+        gray_frames = testpipe.as_ndarray(frame_key=rgb2gray.key_name)
+        self.assertTupleEqual(gray_frames.shape, (n, frame_width,
+                                                  frame_height))
 
 if __name__ == '__main__':
     unittest.main()
