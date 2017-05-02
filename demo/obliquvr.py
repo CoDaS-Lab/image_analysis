fd42ff892e5a7172b3b6b424e408ae1de4d43fdd
Ravi Soji <raviisojitraa@gmail.com>
2017-05-02 17:52:17 -0400

    Got orientation filter and the demo running.

--- demo/obliquvr.py
+++ demo/obliquvr.py
@@ -7,22 +7,26 @@ from matplotlib import pyplot as plt
 from decode import video_decoder as vd
 from extract.orientation_filter import OrientationFilter
 from extract.pipeline import Pipeline
+from skimage.color import rgb2gray
 
 # Test the bowtie filter with sine waves
-img = skimage.io.imread('../test/test_data/testimgs/test1.jpg')[:426, :426, :]
+img = skimage.io.imread('../test1.jpg')[:426, :426, :]
 
 motion_analysis = Pipeline(data=[[img]],
-                           ops=[OrientationFilter('bowtie', 90, 20, 426, .2,
+                           ops=[OrientationFilter('bowtie', 90, 42, 426, .2,
                                                   426, 'triangle')],
                            save_all=True)
 
 pipeline_output = motion_analysis.extract()
 # print(pipeline_output[-1]['seq_features'])
+print(pipeline_output[0]['frame_features']['bowtie_filter'].shape)
 
 # print(motion_analysis.as_ndarray(seq_key='batch_length'))
 # motion_analysis.display()
 # Now, let's access  some of the features extracted from frames
 imgs = [pipeline_output[0]['input'],
         pipeline_output[0]['frame_features']['bowtie_filter']]
-skimage.io.imshow_collection(imgs)
+#skimage.io.imshow_collection(imgs)
+#plt.imshow(rgb2gray(pipeline_output[0]['input']), cmap='gray')
+plt.imshow(pipeline_output[0]['frame_features']['bowtie_filter'], cmap='gray')
 plt.show()
