9e6a6b47df4af72b4297302ebb8cc1427ba3e376
Anderson Reyes <reyesanderson428@gmail.com>
2017-05-04 11:23:37 -0400

    pep8 formatting

--- demo/obliquvr.py
+++ demo/obliquvr.py
@@ -10,7 +10,7 @@ from extract.pipeline import Pipeline
 from skimage.color import rgb2gray
 
 # Test the bowtie filter with sine waves
-#img = skimage.io.imread('../test1.jpg')[:426, :426, :]
+# img = skimage.io.imread('../test1.jpg')[:426, :426, :]
 img = skimage.io.imread('../test2.jpg')[:1000, :1000, :]
 
 motion_analysis = Pipeline(data=[[img]],
@@ -25,15 +25,15 @@ print(pipeline_output[0]['frame_features']['bowtie_filter'].shape)
 # print(motion_analysis.as_ndarray(seq_key='batch_length'))
 # motion_analysis.display()
 # Now, let's access  some of the features extracted from frames
-#img_filtered_amp_spectrum = np.load('filtered_img_amp_spectrum.npy')
+# img_filtered_amp_spectrum = np.load('filtered_img_amp_spectrum.npy')
 
 imgs = [pipeline_output[0]['input'],
         pipeline_output[0]['frame_features']['bowtie_filter']]
 filtered_minus_original = imgs[1] - rgb2gray(imgs[0])
-#skimage.io.imshow_collection(imgs)
-#plt.imshow(rgb2gray(pipeline_output[0]['input']), cmap='gray')
-#plt.imshow(pipeline_output[0]['frame_features']['bowtie_filter'], cmap='gray')
+# skimage.io.imshow_collection(imgs)
+# plt.imshow(rgb2gray(pipeline_output[0]['input']), cmap='gray')
+# plt.imshow(pipeline_output[0]['frame_features']['bowtie_filter'], cmap='gray')
 plt.imshow(filtered_minus_original)
 plt.show()
-#plt.imshow(img_filtered_amp_spectrum)
-#plt.show()
+# plt.imshow(img_filtered_amp_spectrum)
+# plt.show()
