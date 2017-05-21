0fc3a3eebf2f4d81cd4f84741ad513ff9a20e249
Anderson Reyes <reyesanderson428@gmail.com>
2017-05-21 15:09:43 -0400

    added bootstrap theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@
 #
 import os
 import sys
+import sphinx_bootstrap_theme
 sys.path.insert(0, os.path.abspath('../..'))
 
 
@@ -87,7 +88,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -98,8 +99,71 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
+html_theme_options = {
+    # # Navigation bar title. (Default: ``project`` value)
+    # 'navbar_title': "Demo",
+
+    # # Tab name for entire site. (Default: "Site")
+    # 'navbar_site_name': "Site",
+
+    # # A list of tuples containing pages or urls to link to.
+    # # Valid tuples should be in the following forms:
+    # #    (name, page)                 # a link to a page
+    # #    (name, "/aa/bb", 1)          # a link to an arbitrary relative url
+    # #    (name, "http://example.com", True) # arbitrary absolute url
+    # # Note the "1" or "True" value above as the third argument to indicate
+    # # an arbitrary url.
+    # 'navbar_links': [
+    #     ("Examples", "examples"),
+    #     ("Link", "http://example.com", True),
+    # ],
+
+    # Render the next and previous page links in navbar. (Default: true)
+    'navbar_sidebarrel': True,
+
+    # Render the current pages TOC in the navbar. (Default: true)
+    'navbar_pagenav': True,
+
+    # Tab name for the current pages TOC. (Default: "Page")
+    'navbar_pagenav_name': "Page",
+
+    # Global TOC depth for "site" navbar tab. (Default: 1)
+    # Switching to -1 shows all levels.
+    'globaltoc_depth': 2,
+
+    # Include hidden TOCs in Site navbar?
+    #
+    # Note: If this is "false", you cannot have mixed ``:hidden:`` and
+    # non-hidden ``toctree`` directives in the same page, or else the build
+    # will break.
+    #
+    # Values: "true" (default) or "false"
+    'globaltoc_includehidden': "true",
+
+    # HTML navbar class (Default: "navbar") to attach to <div> element.
+    # For black navbar, do "navbar navbar-inverse"
+    'navbar_class': "navbar navbar-inverse",
+
+    # Fix navigation bar to top of page?
+    # Values: "true" (default) or "false"
+    'navbar_fixed_top': "true",
+
+    # Location of link to source.
+    # Options are "nav" (default), "footer" or anything else to exclude.
+    'source_link_position': "nav",
+
+    # Bootswatch (http://bootswatch.com/) theme.
+    #
+    # Options are nothing (default) or the name of a valid theme
+    # such as "amelia" or "cosmo".
+    'bootswatch_theme': "united",
+
+    # Choose Bootstrap version.
+    # Values: "3" (default) or "2" (in quotes)
+    'bootstrap_version': "3",
+}
 
 # -- Options for HTMLHelp output ------------------------------------------
 
