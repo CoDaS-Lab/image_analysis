e99b21a8da598fe8b97fba9d3ba51b71efccfa74
Anderson Reyes <reyesanderson428@gmail.com>
2017-06-06 22:35:39 -0400

    updated docs to automate docs updating

--- gen-docs.py
+++ gen-docs.py
@@ -2,24 +2,33 @@ import os
 import sys
 import time
 import glob
+import shutil
 from os.path import basename
 from subprocess import Popen, PIPE, call
+
+
+def movefiles(regex, dest):
+    for fname in glob.glob(regex):
+        shutil.move(fname, dest)
 
 
 def delfiles(regex, ignore=None):
     if ignore is None:
         ignore = ('')
 
-    for CleanUp in glob.glob(regex):
-        if not CleanUp.endswith(ignore):
-            os.remove(CleanUp)
+    for fname in glob.glob(regex):
+        if not basename(fname).endswith(ignore):
+            if os.path.isdir(fname):
+                shutil.rmtree(fname)
+            elif os.path.isfile(fname):
+                os.remove(fname)
 
-call(['git', 'checkout', 'gh-pages'])
+delfiles('./**', ignore=('gen-docs.py'))
+call(['git', 'checkout', 'origin/master', '--', '.'])
 print('--- BUILD DOCS ---')
 
 # clean old *.rst source files since sphinx doesn't do this automatically
-excludes = ('index.rst')
-delfiles(os.getcwd() + '/docs/source/*.rst', ignore=excludes)
+delfiles('.any/docs/source/*.rst', ignore=('index.rst'))
 call(['sphinx-apidoc', '-f', '-o', './docs/source', '.', 'test', 'gen-docs.py',
       'setup.py'])
 
@@ -32,3 +41,12 @@ call(['make', 'clean'])
 # rebuild html
 call(['make', 'html'])
 os.chdir(os.path.abspath('../'))
+
+# make all html files in current dir and remove docs
+delfiles('./*', ignore=('gen-docs.py', 'docs'))
+movefiles('./docs/build/html/**', './')
+shutil.rmtree('./docs')
+
+call(['git', 'add', '-A'])
+call(['git', 'commit', '-m', 'updated docs'])
+
